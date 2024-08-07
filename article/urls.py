ecc0ef83a3391a141d1af33330bbbee1917786e9
seref081 <157972665+seref081@users.noreply.github.com>
2024-08-07 22:40:57 +0300

    Add files via upload

---
+++ article/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+
+app_name="article"
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('addarticle/', views.addArticle, name="addarticle"),
+    path('article/<int:id>', views.detail, name="detail"),
+    path('', views.articles, name="articles"),
+    path('comment/<int:id>', views.addComment, name="comment"),
+
+
+
+
+]
